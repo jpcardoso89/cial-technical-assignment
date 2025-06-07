6c0e6302fe2d3664b8e9a52645a4ff3d6b91e8f0
João Paulo Cardoso <jpcardoso89@gmail.com>
2025-06-07 10:18:38 -0300

    create domain model

---
+++ app/src/domain/model/competitor.py
@@ -0,0 +1,4 @@
+class Competitor:
+    def __init__(self, name: str, market_cap: MarketCap):
+        self.name = name
+        self.market_cap = market_cap
---
+++ app/src/domain/model/stock_values.py
@@ -0,0 +1,6 @@
+class StockValues:
+    def __init__(self, open: float, high: float, low: float, close: float):
+        self.open = open
+        self.high = high
+        self.low = low
+        self.close = close
